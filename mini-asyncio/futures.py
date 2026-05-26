7b8379be1acc8c0f697a60c50201daa638c55912
Aman-apt <amanyadav665078@gmail.com>
2026-05-26 16:37:43 +0530

    A Simple Implementation of future

--- mini-asyncio/futures.py
+++ mini-asyncio/futures.py
@@ -1,39 +1,50 @@
-
-#Note: Not a proper implementation just a rough example. 
-# Things to improve:
-# Add a proper Exception handling, Logging and make it robuts to handle edge cases
+# Futures.py
+# Futures is the underlying 
 
 class Future:
-    """
-    Future Class that is responsible for handling the coroutine object
-    and yield the future out
-    """
-    _loop = None
-
-    def __init__(self, loop=None):
+    def __init__(self):
         self._result = None
         self._done = False
-        self._callbacks = []
-    
-    def get_loop(self):
-        """Return the event loop the future is bound to"""
-        loop = self._loop
-        if loop is None:
-            raise RuntimeError("Future object is Not initialized")
-        return loop
+        self._callback = []
 
     def set_result(self, value):
-        """
-        set the callback as a result
-        """
+        if self._done:
+            raise Exception("Future already done")
         self._result = value
         self._done = True
-        for cb in self._callbacks: #fire everyone waiting(sheduling)
-            cb(self)
-    
-    def add_done_callbacks(self, fn):
-        self._callbacks.append(fn)
-    
+
+        # Notifies the coroutine that futures has finished
+        for callback in self._callback:
+            callback(self)
+
     def result(self):
+        if not self._done:
+            raise Exception("Not ready yet")
         return self._result
-    
+
+    def add_done_callback(self, fn):
+        if self._done: # fire all the callbacks immediately
+            fn(self)
+        else:
+            self._callback.append(fn)
+
+    def done(self):
+        return self._done
+
+    def __await__(self):
+        if not self._done:
+            yield self
+        return self._result()
+
+f = Future()
+
+def on_done(future):
+    print(f"Callback fired! Result {future.result()}")
+#paste what you get. Don't move on until this works — every single thing we build after this sits on top of
+f.add_done_callback(on_done)
+print(f"Done before set_result: {f.done()}")
+f.set_result(230)
+f.set_result(32)
+print(f"Done after the set_result {f.done()}")
+print(f"Result: {f.result()}")
+
